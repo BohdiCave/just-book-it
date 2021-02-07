50cbd9e69df0c05d33c7c0edca9852dec0315905
Bohdan Pechenyak <bohdan.pechenyak@gmail.com>
2021-02-07 05:53:32 -0500

    debug

--- models/Book.js
+++ models/Book.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema;
 
 const BookSchema = new Schema({
   title: { type: String, trim: true, required: true },
-  authors: [{ type: String, unique: true, required: true }],
+  authors: { type: String, unique: true, required: true },
   description: String,
   image: { type: String, match: [/http.+/, "Not a valid URL"]},
   link: { type: String, match: [/http.+/, "Not a valid URL"]}
