742c97d32d2d982fa3321c9ae6c227197d462978
Bohdan Pechenyak <bohdan.pechenyak@gmail.com>
2021-02-07 05:21:01 -0500

    correcting imports in CRA

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import API from "../utils/API";
+import API from "../utils/api";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import Main from "../components/Main";
